20655c3dbb01216cecf9a2e15901a369d8340ca9
CharuniKaushalya <snkaushi@gmail.com>
2015-10-31 19:33:19 +0530

    Connect to controllers by url

--- app/core/App.php
+++ app/core/App.php
@@ -7,7 +7,21 @@ class App{
 	protected $params= [];
 	public function __construct(){
 		//echo "OK";
-		print_r($this->parseUrl());
+		//print_r($this->parseUrl());
+		$url = $this->parseUrl();
+		print_r($url);
+		if(file_exists('../app/controllers/'.$url[0].'.php')){
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+		require_once '../app/controllers/'.$this->controller.'.php';
+		$this->controller = new $this->controller;
+		if(isset($url[1])){
+			if(method_exists($this->controller, $url[1])){
+				echo 'OK';
+			}
+		}
+
 	}
 
 	public function parseUrl(){
